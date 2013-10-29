53313d39c6fb6ba2ac7468a49df1261f136de7ee
Adam Miller <acm6603@gmail.com>
2013-10-29 17:15:41 -0400

    adding bundler exec and the python directory

---
+++ python/aliases.zsh
@@ -0,0 +1,3 @@
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/share/python/virtualenv
+source /usr/local/share/python/virtualenvwrapper.sh
--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/Developer/usr/bin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin/:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/Developer/usr/bin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:/usr/local/share/python:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
